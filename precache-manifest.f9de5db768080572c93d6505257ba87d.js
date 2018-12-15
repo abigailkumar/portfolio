486b6f671aa8205df4e1c101ce7499074bf5dc3b
babykooms <babykooms@users.noreply.github.com>
2018-12-14 21:24:45 -0600

    Updates

---
+++ precache-manifest.f9de5db768080572c93d6505257ba87d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0ef49fa071f39949d6ec39894be3ffd",
+    "url": "/portfolio/static/media/apparelshoot1.e0ef49fa.jpg"
+  },
+  {
+    "revision": "1c79e1f3116c3c367e5c",
+    "url": "/portfolio/static/css/main.ebd74b1d.chunk.css"
+  },
+  {
+    "revision": "05b917add8abcc84f1d9",
+    "url": "/portfolio/static/js/1.05b917ad.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2a9a8f0aa512592dcfed97e89342200a",
+    "url": "/portfolio/static/media/pixelPractice.2a9a8f0a.png"
+  },
+  {
+    "revision": "1c79e1f3116c3c367e5c",
+    "url": "/portfolio/static/js/main.1c79e1f3.chunk.js"
+  },
+  {
+    "revision": "da986f2d4b405dce9e66d0935b6dd510",
+    "url": "/portfolio/static/media/caase-banner2.da986f2d.jpg"
+  },
+  {
+    "revision": "71334a48168f9d16dd8f9026493e1c74",
+    "url": "/portfolio/static/media/abigail.71334a48.jpg"
+  },
+  {
+    "revision": "1993c568374c3f8e5b8203a0a6f8c82e",
+    "url": "/portfolio/static/media/aquasite.1993c568.jpg"
+  },
+  {
+    "revision": "0a4ddab80cb1d49c65e4325b8e5a69a6",
+    "url": "/portfolio/static/media/benchmarkAnalysis.0a4ddab8.pdf"
+  },
+  {
+    "revision": "898a4444944b7bcb26aaf269bc72f355",
+    "url": "/portfolio/index.html"
+  }
+];
